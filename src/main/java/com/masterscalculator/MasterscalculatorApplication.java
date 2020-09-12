2f2e7edb80529ea56efe7716d9a81cfb4a02b62d
TrainerFromHoenn <Classical-Maiku@users.noreply.github.com>
2020-09-12 16:29:29 +0200

    Modifiche

--- src/main/java/com/masterscalculator/MasterscalculatorApplication.java
+++ src/main/java/com/masterscalculator/MasterscalculatorApplication.java
@@ -3,11 +3,15 @@ package com.masterscalculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import java.util.Arrays;
+
 @SpringBootApplication
 public class MasterscalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MasterscalculatorApplication.class, args);
 	}
+
 
 }
